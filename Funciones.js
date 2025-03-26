d73fb61392004578d44eb5982573967334def12f
OrlinSalgado <60558793+OrlinSalgado@users.noreply.github.com>
2025-03-25 22:22:21 -0600

    agregado pruebas de funciones con circulo y rectangulo

--- Funciones.js
+++ Funciones.js
@@ -3,3 +3,27 @@ function calTriangulo (base, altura, ladoA, ladoB) {
   const perimetro = parseFloat(base) + parseFloat(ladoA) + parseFloat(ladoB);
   return {area, perimetro};
 }
+
+function mostrarFormulario(tipo) {
+  if(tipo=='areaCirculo'){
+    document.getElementById("formularioAreaCirculo").style.display = "block";
+  }
+  else if(tipo=='areaRectangulo'){
+    document.getElementById("formularioAreaRectangulo").style.display = "block";
+  }
+  
+}
+
+function calcularArea(tipo) {
+  if (tipo == 'circulo') {
+    let radio = document.getElementById("radio").value;
+    let area = Math.PI * Math.pow(radio, 2);
+    document.getElementById("resultado").innerHTML = "El Área es: " + area.toFixed(2);
+  }
+  else if(tipo == 'rectangulo'){
+    let altura = document.getElementById("alturaRectangulo").value;
+    let ancho = document.getElementById("anchoRectangulo").value;
+    let areaRectangulo = altura*ancho;
+    document.getElementById("resultadoAreaRectangulo").innerHTML = "El Área es: " + areaRectangulo.toFixed(2);
+  }
+}
